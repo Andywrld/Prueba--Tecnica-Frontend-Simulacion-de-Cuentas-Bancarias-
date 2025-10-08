18bd8b45c25cf51502723b4d0e03266737c16b10
Andy Torres <andy18wrld@gmail.com>
2025-10-07 20:33:01 -0400

    feat:Creada la instacia de react-query

---
+++ src/lib/query-client.ts
@@ -0,0 +1,17 @@
+// src/lib/react-query.ts
+import { QueryClient } from '@tanstack/react-query';
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      retry: 1,
+      networkMode: 'online',
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: true,
+    },
+    mutations: {
+      retry: 0,
+    },
+  },
+});
