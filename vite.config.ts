77f2cfe556c4f1000633969c82b4d0768ffc1490
Andy Torres <andy18wrld@gmail.com>
2025-10-07 20:06:30 -0400

    feat: agregar configuración de alias en Vite para mejorar la resolución de módulos

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@layout': path.resolve(__dirname, 'src/layout'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@api': path.resolve(__dirname, 'src/api'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@features': path.resolve(__dirname, 'src/features'),
+      '@store': path.resolve(__dirname, 'src/store'),
+    },
+  },
 });
