a9b1280c19a078e279b6d3105a8a95aa3c183f0e
Andy Torres <andy18wrld@gmail.com>
2025-10-07 21:30:07 -0400

    fix:Corregido error de opmtimist de data

--- src/features/accounts/hook/useUpdateAccount.ts
+++ src/features/accounts/hook/useUpdateAccount.ts
@@ -1,6 +1,7 @@
 // src/features/accounts/hook/useUpdateBalance.ts
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { UpdateBalanceAccount } from '../service/UpdateAccount';
+import type { Account } from '../types/account.type';
 
 export const useUpdateBalance = () => {
   const queryClient = useQueryClient();
@@ -9,8 +10,32 @@ export const useUpdateBalance = () => {
     mutationFn: ({ id, newBalance }: { id: string; newBalance: number }) =>
       UpdateBalanceAccount(id, { balance: newBalance }),
 
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['account'] });
+    onMutate: async ({ id, newBalance }) => {
+      await queryClient.cancelQueries({ queryKey: ['accounts'] });
+
+      const previousAccounts = queryClient.getQueryData<Account[]>([
+        'accounts',
+      ]);
+
+      queryClient.setQueryData<Account[]>(['accounts'], (old) =>
+        old
+          ? old.map((account) =>
+              account.id === id ? { ...account, balance: newBalance } : account
+            )
+          : []
+      );
+
+      return { previousAccounts };
+    },
+
+    onError: (_error, _variables, context) => {
+      if (context?.previousAccounts) {
+        queryClient.setQueryData(['accounts'], context.previousAccounts);
+      }
+    },
+
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['accounts'] });
     },
   });
 };
